5a6c5fa051542919c756e934aaf618b250363765
Senai Mehari <Senai.mehari@stu.ocsb.ca>
2023-09-22 02:24:22 +0000

    the helps calculate the are and perimeter of a rectangle

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Senai Mehari
+# Date:09 21,2023
+# This program was made to do basic math
+
+def main ():
+    print("2-1= {}". format(2-1))
+    print("5+3= {}". format(5+3))
+    print("4*1= {}". format(4*1))
+    print("7/2= {}". format(7/2))
+    print("5+6/2= {}". format(5+6/2))
+    print("21^3= {}". format(21^3))
+    print("5**4= {}". format(5**4))
+    print("13**8.7= {}". format(13**8.7))
+
+if __name__ == "__main__":
+    main()
